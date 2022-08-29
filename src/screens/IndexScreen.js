ae1050c4f6c320c8cfaee7765b9c4ea344c2187a
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-08-29 17:10:00 -0400

    navigating from header

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -38,6 +38,16 @@ const IndexScreen = ({ navigation }) => {
   );
 };
 
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+        <AntDesign name="plus" style={styles.header} color="black" />
+      </TouchableOpacity>
+    ),
+  };
+};
+
 const styles = StyleSheet.create({
   row: {
     flexDirection: "row",
@@ -52,6 +62,10 @@ const styles = StyleSheet.create({
   },
   icon: {
     fontSize: 24,
+  },
+  header: {
+    fontSize: 24,
+    marginRight: 10,
   },
 });
 
