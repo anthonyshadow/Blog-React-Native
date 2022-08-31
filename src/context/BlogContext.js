23c54b654693569f3cc352df70c74e8c71847956
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-08-31 12:28:08 -0400

    can edit posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,6 +13,10 @@ const blogReducer = (state, action) => {
           content: action.payload.content,
         },
       ];
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     default:
       return state;
   }
@@ -31,8 +35,15 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    callback();
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   [{ title: "Test Post", content: "Test Content", id: 1 }]
 );
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,43 +1,24 @@
-import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
+  const id = navigation.getParam("id");
+  const { state, editBlogPost } = useContext(Context);
 
-  const blogPost = state.find(
-    (blogPost) => blogPost.id === navigation.getParam("id")
-  );
-
-  const [title, setTitle] = useState(blogPost.title);
-  const [conter, setContent] = useState(blogPost.content);
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
   return (
-    <View>
-      <Text style={styles.label}>Edit Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(newTitle) => setTitle(newTitle)}
-      />
-    </View>
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.navigate("Index"));
+      }}
+    />
   );
 };
 
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "black",
-    marginBottom: 15,
-    padding: 5,
-    margin: 5,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-    marginLeft: 5,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
